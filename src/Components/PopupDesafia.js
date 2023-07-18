b1d5e73c5ed5ffdf5297b076077083719620f4ff
santiagofarriols <santiagofarriols@gmail.com>
2023-07-18 14:51:30 +0200

    a

--- src/Components/PopupDesafia.js
+++ src/Components/PopupDesafia.js
@@ -43,7 +43,7 @@ function PopupDesafia({ isOpen, closeModal }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (uid) {
+e    if (uid) {
       const dare = {
         title,
         description,
